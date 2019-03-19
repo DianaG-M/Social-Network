2d0227eed499a7b0ce5143bfbe7bf826265236f0
Diana <aries_di@hotmail.com>
2019-03-19 09:00:41 -0600

    Subiendo para bajar

--- src/js/rutas.js
+++ src/js/rutas.js
@@ -1,12 +1,6 @@
 (function () {
-    
     library.getID('view').redirect().road('/', './views/home.html', null, null)
-           .road('/crear-contacto', 
-                 './views/publication/create.html', 
-                 'contact', 
-                 function(){
-                    library.getID('create-contact').noSubmit();
-                 })
-           .road('/leer-contacto', './views/publication/read.html', 'contact', null)
-           .road('/actualizar-contacto', './views/publication/update.html','contact',null)
+           .road('/crear-contacto', './views/publication/create.html', null, null)
+           .road('/leer-contacto', './views/publication/read.html', null,null)
+           .road('/actualizar-contacto', './views/publication/update.html',null,null)
 })();
